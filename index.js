403240c41a27e1f60ddb259847894a819842d147
Joz-byte <josephinezelleke@gmail.com>
2023-05-28 13:14:54 +0200

    Updated JS to include current location

--- index.js
+++ index.js
@@ -47,6 +47,28 @@ function updateCity(event) {
         </div>`;
 }
 
+function updateCity(event) {
+  let cityTimezone = event.target.value;
+  if (cityTimezone === "current") {
+    cityTimezone = moment.tz.guess();
+  }
+  let cityName = cityTimezone.replace("_", " ").replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimezone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+        <div class="city">
+        <div>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format(
+          "h:mm:ss "
+        )}<small>${cityTime.format("A")}</small></div>
+            </div>`;
+  setTimeout(() => {
+    updateCity(event);
+  }, 1000);
+}
 updateTime();
 setInterval(updateTime, 1000);
 
